de0a52804c18c410a86486d30b98844d5f03890a
Ethan Hulse <ethanhulse.work@gmail.com>
2025-01-16 15:38:41 -0700

    Refactored code to allow text files as input

--- steGo.go
+++ steGo.go
@@ -1,165 +1,194 @@
 package main
 
-
 import (
-  "bytes"
-  "fmt"
-  "image"
-  "image/color"
-  "image/png"
-  "os"
-  "strings"
+    "bytes"
+    "encoding/binary"
+    "fmt"
+    "image"
+    "image/color"
+    "image/png"
+    "os"
+    "strings"
 )
 
-const delimiter = "##END##"
-
-
-// Convert input string to binary for encoding
-// Private helper function for encodeMessage function
-func stringToBinary(s string) string {
-  var binaryString string
-  for _, c := range s {
-    binaryString += fmt.Sprintf("%08b", c)
-  }
-  return binaryString
-}
-
-func binaryToString(binary string) string {
-  var text string
-  for i := 0; i < len(binary); i += 8 {
-    if i+8 > len(binary){
-      break
-    }
-    var char byte
-    fmt.Sscanf(binary[i:i+8], "%b", &char)
-    text += string(char)
-  }
-  return text
-}
-// Encode binary message into file
-func encodeMessage(inputFile, outputFile, message string) error {
-  //open input image
-
-  file, err := os.Open(inputFile)
-  if err != nil {
-    return err
-  }
-  defer file.Close()
-
-  img, err := png.Decode(file)
-  if err != nil {
-    return err
-  }
-
-  //Convert message to binary and add a delimiter
-
-  binaryMessage := stringToBinary(message + delimiter)
-
-  bounds := img.Bounds()
-  width, height := bounds.Max.X, bounds.Max.Y
-
-  //Create the new image with the encoded and modified pixels
-
-  newImg := image.NewRGBA(bounds)
-
-  bitIndex := 0
-  for y := 0; y < height; y++ {
-    for x := 0; x < width; x++ {
-      r, g, b, a := img.At(x, y).RGBA()
-
-      if bitIndex < len(binaryMessage) {
-        //Modify the LSB of the red channel
-        r = (r &^ 1) | uint32(binaryMessage[bitIndex]-'0')
-        bitIndex++
-      }
-
-      newImg.Set(x,y, color.RGBA{uint8(r), uint8(g), uint8(b), uint8(a)})
-    }
-  }
-
-  //Save the modified image
-  outFile, err := os.Create(outputFile)
-  if err != nil {
-    return err
-  }
-  defer outFile.Close()
-
-  return png.Encode(outFile, newImg)
-}
-
-//Decode a message from a stego image
-
-func decodeMessage(inputFile string)(string, error){
-  //open inout image
-  file, err := os.Open(inputFile)
-  if err != nil {
-    return "", err
-  
-  }
-  defer file.Close()
-
-  img, err := png.Decode(file)
-  if err != nil {
-    return "", err
-  }
-
-  bounds := img.Bounds()
-  width, height := bounds.Max.X, bounds.Max.Y
-
-  var binaryMessage bytes.Buffer
-
-  for y := 0; y < height; y++ {
-    for x := 0; x < width; x++ {
-      r, _, _, _ := img.At(x, y).RGBA()
-      binaryMessage.WriteString(fmt.Sprintf("%b", r&1))
-
-      //Check if we have reached Delim string
-      if strings.Contains(binaryToString(binaryMessage.String()), delimiter) {
-        message := binaryToString(binaryMessage.String())
-        return strings.Split(message, delimiter)[0], nil
-      }
-    }
-  }
-  return "", fmt.Errorf("No hidden message found")
-}
-
-// CLI Interface
-
+// Main function to handle CLI arguments
 func main() {
-  if len(os.Args) <  5 {
-    fmt.Println("Usage:")
-    fmt.Println(" Encoding: ./stego -mode encode -input <inputfile.png> -output output.png -message \"secret message\"")
-    fmt.Println(" Decoding: ./stego -mode decode -input <encodedfile.png>")
-    return
-  }
-
-  mode := os.Args[2]
-  inputFile := os.Args[4]
-
-  switch mode {
-
-  case "encode":
-    if len(os.Args) < 8 {
-      fmt.Println("Missing arguments for encoding.")
-      return
+    if len(os.Args) < 3 {
+        fmt.Println("Usage:")
+        fmt.Println("  Encode: steGo encode input.png output.png \"Hidden message\"")
+        fmt.Println("  Encode with file: steGo encode input.png output.png -file secret.txt")
+        fmt.Println("  Decode: steGo decode output.png")
+        fmt.Println("  Decode to file: steGo decode output.png -file extracted.txt")
+        return
     }
-    outputFile := os.Args[6]
-    message := os.Args[8]
-    err := encodeMessage(inputFile, outputFile, message)
+
+    mode := os.Args[1] // "encode" or "decode"
+    input := os.Args[2] // Input PNG
+    output := ""
+    message := ""
+
+    if mode == "encode" {
+        if len(os.Args) < 4 {
+            fmt.Println("Error: Missing output file.")
+            return
+        }
+        output = os.Args[3] // Output PNG
+
+        if len(os.Args) > 4 && os.Args[4] == "-file" {
+            if len(os.Args) < 6 {
+                fmt.Println("Error: Missing input file for encoding.")
+                return
+            }
+            filePath := os.Args[5] // File to read data from
+            data, err := os.ReadFile(filePath)
+            if err != nil {
+                fmt.Println("Error reading file:", err)
+                return
+            }
+            message = string(data)
+        } else {
+            message = os.Args[4] // Raw text message
+        }
+
+        encodeMessage(input, output, message)
+    } else if mode == "decode" {
+        extracted := decodeMessage(input)
+
+        if len(os.Args) > 3 && os.Args[3] == "-file" {
+            if len(os.Args) < 5 {
+                fmt.Println("Error: Missing output file for extracted text.")
+                return
+            }
+            filePath := os.Args[4]
+            err := os.WriteFile(filePath, []byte(extracted), 0644)
+            if err != nil {
+                fmt.Println("Error writing to file:", err)
+                return
+            }
+            fmt.Println("Hidden message extracted to:", filePath)
+        } else {
+            fmt.Println("Hidden message:", extracted)
+        }
+    }
+}
+
+// Encode a hidden message inside a PNG image
+func encodeMessage(inputFile, outputFile, message string) {
+    file, err := os.Open(inputFile)
     if err != nil {
-      fmt.Println("Encoding error: ", err)
-      return
+        fmt.Println("Error opening image:", err)
+        return
     }
-    fmt.Println("Message successfully encoded!")
-  case "decode":
-    message, err := decodeMessage(inputFile)
-    if err != nil {
-      fmt.Println("Decoding error: ", err)
-      return
-    }
-    fmt.Println("Decoded Message:", message)
+    defer file.Close()
 
-  default:
-    fmt.Println("Invalid mdoe, please use 'encode' or 'decode'.")
-  }
+    img, _, err := image.Decode(file)
+    if err != nil {
+        fmt.Println("Error decoding image:", err)
+        return
+    }
+
+    bounds := img.Bounds()
+    imgWidth := bounds.Dx()
+    imgHeight := bounds.Dy()
+    totalPixels := imgWidth * imgHeight
+
+    // Convert message to binary
+    messageBytes := []byte(message)
+    messageBytes = append(messageBytes, 0) // Null terminator to signal end of message
+    messageLength := len(messageBytes)
+
+    if messageLength*8 > totalPixels {
+        fmt.Println("Error: Message is too large to fit in the image!")
+        return
+    }
+
+    outFile, err := os.Create(outputFile)
+    if err != nil {
+        fmt.Println("Error creating output file:", err)
+        return
+    }
+    defer outFile.Close()
+
+    png.Encode(outFile, hideMessage(img, messageBytes))
+    fmt.Println("Message successfully embedded in", outputFile)
+}
+
+// Extract a hidden message from a PNG image
+func decodeMessage(inputFile string) string {
+    file, err := os.Open(inputFile)
+    if err != nil {
+        fmt.Println("Error opening image:", err)
+        return ""
+    }
+    defer file.Close()
+
+    img, _, err := image.Decode(file)
+    if err != nil {
+        fmt.Println("Error decoding image:", err)
+        return ""
+    }
+
+    extractedBytes := extractHiddenMessage(img)
+    return string(extractedBytes)
+}
+
+// Hide a message inside an image using LSB
+func hideMessage(img image.Image, messageBytes []byte) *image.RGBA {
+    bounds := img.Bounds()
+    newImg := image.NewRGBA(bounds)
+
+    msgIndex := 0
+    bitIndex := 0
+
+    for y := bounds.Min.Y; y < bounds.Max.Y; y++ {
+        for x := bounds.Min.X; x < bounds.Max.X; x++ {
+            originalColor := img.At(x, y)
+            r, g, b, a := originalColor.RGBA()
+            r, g, b = r>>8, g>>8, b>>8 // Convert to 8-bit values
+
+            // Modify LSB of red channel to store message bits
+            if msgIndex < len(messageBytes) {
+                bit := (messageBytes[msgIndex] >> (7 - bitIndex)) & 1
+                r = (r & 0xFE) | bit
+
+                bitIndex++
+                if bitIndex == 8 {
+                    bitIndex = 0
+                    msgIndex++
+                }
+            }
+
+            newImg.Set(x, y, color.RGBA{uint8(r), uint8(g), uint8(b), uint8(a >> 8)})
+        }
+    }
+    return newImg
+}
+
+// Extract a hidden message from an image using LSB
+func extractHiddenMessage(img image.Image) []byte {
+    bounds := img.Bounds()
+    var extractedBits []byte
+    var currentByte byte = 0
+    bitIndex := 0
+
+    for y := bounds.Min.Y; y < bounds.Max.Y; y++ {
+        for x := bounds.Min.X; x < bounds.Max.X; x++ {
+            r, _, _, _ := img.At(x, y).RGBA()
+            r = r >> 8 // Convert to 8-bit value
+
+            // Extract LSB from red channel
+            currentByte = (currentByte << 1) | byte(r&1)
+            bitIndex++
+
+            if bitIndex == 8 {
+                if currentByte == 0 { // Null terminator signals end of message
+                    return extractedBits
+                }
+                extractedBits = append(extractedBits, currentByte)
+                bitIndex = 0
+                currentByte = 0
+            }
+        }
+    }
+    return extractedBits
 }
